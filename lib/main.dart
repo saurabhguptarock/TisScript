3648c1f7c002a097d070f2f0fabda2ca55518311
saurabh gupta <saurabhguptarock119@gmail.com>
2020-03-29 23:15:44 +0530

    added search feature

--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,7 @@ class _MyHomePageState extends State<MyHomePage> {
       List.generate(availableNodes.length, (index) => Colors.transparent);
   bool _showNodeMenu = false;
   List<Widget> _nodes = [];
+  String _searchQuery = '';
 
   @override
   Widget build(BuildContext context) {
@@ -67,7 +68,6 @@ class _MyHomePageState extends State<MyHomePage> {
             _showNodeMenu = false;
           });
       },
-      onDoubleTap: () {},
       onPanUpdate: (details) {
         // setState(() {
         //   offset[_nodes.length - 1] = Offset(
@@ -108,15 +108,17 @@ class _MyHomePageState extends State<MyHomePage> {
                             child: TextField(
                               autofocus: true,
                               keyboardType: TextInputType.text,
-                              autocorrect: true,
                               cursorColor: Colors.white,
                               decoration: InputDecoration.collapsed(
                                   border: InputBorder.none,
                                   hintStyle: TextStyle(
                                       fontSize: 12, color: Colors.white),
                                   hintText: 'Search...'),
-                              onChanged: (s) {},
-                              enableInteractiveSelection: true,
+                              onChanged: (s) {
+                                setState(() {
+                                  _searchQuery = s;
+                                });
+                              },
                               minLines: 1,
                               maxLines: 1,
                               style: TextStyle(
@@ -132,36 +134,41 @@ class _MyHomePageState extends State<MyHomePage> {
                         height: 252,
                         child: ListView.builder(
                           itemCount: availableNodes.length,
-                          itemBuilder: (ctx, i) => InkWell(
-                            onTap: () {
-                              setState(() {
-                                _showNodeMenu = !_showNodeMenu;
-                              });
-                              _nodes.add(availableNodes[i]
-                                  .toWidget(offsetX: left, offsetY: top));
-                              _hovercolor[i] = Colors.transparent;
-                            },
-                            child: MouseRegion(
-                              onHover: (details) {
-                                setState(() {
-                                  _hovercolor[i] = Color(0xff4E76B7);
-                                });
-                              },
-                              onExit: (details) {
-                                setState(() {
-                                  _hovercolor[i] = Colors.transparent;
-                                });
-                              },
-                              child: Container(
-                                color: _hovercolor[i],
-                                width: MediaQuery.of(context).size.width,
-                                child: Text(
-                                  availableNodes[i].name,
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ),
-                            ).showPointerOnHover,
-                          ),
+                          itemBuilder: (ctx, i) => availableNodes[i]
+                                  .name
+                                  .toLowerCase()
+                                  .contains(_searchQuery.toLowerCase())
+                              ? InkWell(
+                                  onTap: () {
+                                    setState(() {
+                                      _showNodeMenu = !_showNodeMenu;
+                                    });
+                                    _nodes.add(availableNodes[i]
+                                        .toWidget(offsetX: left, offsetY: top));
+                                    _hovercolor[i] = Colors.transparent;
+                                  },
+                                  child: MouseRegion(
+                                    onHover: (details) {
+                                      setState(() {
+                                        _hovercolor[i] = Color(0xff4E76B7);
+                                      });
+                                    },
+                                    onExit: (details) {
+                                      setState(() {
+                                        _hovercolor[i] = Colors.transparent;
+                                      });
+                                    },
+                                    child: Container(
+                                      color: _hovercolor[i],
+                                      width: MediaQuery.of(context).size.width,
+                                      child: Text(
+                                        availableNodes[i].name,
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                    ),
+                                  ).showPointerOnHover,
+                                )
+                              : null,
                         ),
                       ),
                     ],
